27ea458c683b859d0de61f02695d33fe3e2f0fee
Handlecar <zhaoguoqing@handlecar.com>
2016-12-12 01:45:56 +0800

    pageViewCongtroller 浏览 PDF 文件

---
+++ PDF/PDF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDF
+//
+//  Created by Ais on 2016/12/9.
+//  Copyright © 2016年 Ais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
